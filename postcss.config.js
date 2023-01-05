7501698ad410be4183e35f120c1fe5ca159e6a68
yesnoruly <karvatskij.roma@gmail.com>
2023-01-05 19:10:42 +0200

    fix: css import

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,18 @@
+const fs = require("fs")
+const postcss = require("postcss")
+const atImport = require("postcss-import")
+
+const css = fs.readFileSync("src/styles/style.css", "utf8")
+
 module.exports = {
     "plugins": [
-        require("postcss-import"),
+        require("postcss-import")(
+            postcss()
+                .use(atImport())
+                .process(css, {
+                    from: "src/styles/style.css"
+                })
+        ),
         require("autoprefixer")
     ]
 }

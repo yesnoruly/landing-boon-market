7811391cfd7bfa76a73f9c7e55a0dbb4c488e3dd
yesnoruly <karvatskij.roma@gmail.com>
2023-01-13 09:23:35 +0200

    .

--- src/js/header-scrolling.js
+++ src/js/header-scrolling.js
@@ -5,7 +5,7 @@ window.addEventListener('scroll', windowScroll);
 function windowScroll() {
     const distance = header.getBoundingClientRect().top + header.ownerDocument.defaultView.scrollY; // Get the distance between header and Y-coordinate (viewport)
 
-    if (distance > 50) {
+    if (distance > 1) {
         header.classList.add('header--scrolled');
     } else {
         header.classList.remove('header--scrolled')
